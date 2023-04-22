2122a60b3995dd707d0f6cbb3b5471c2315fa40e
naulitaA <naulitarns@gmail.com>
2023-04-22 10:51:56 +0700

    newest update for login & register page

--- login.js
+++ login.js
@@ -1,36 +1,27 @@
-registerform.addEventListener("submit", (e) =>{
-    const registerform = document.getElementById("registerform")
+loginform.addEventListener("submit", (e) =>{
+    const loginform = document.getElementById("loginform")
     const username = document.getElementById("username")
     const password = document.getElementById("password")
-    const address = document.getElementById("address")
     const errorusername = document.getElementById("errorusername")
     const errorpassword = document.getElementById("errorpassword")
-    const erroraddress = document.getElementById("erroraddress")
 
     e.preventDefault()
     let flag = 0
 
     if (username.value.length < 5){
-        errorusername.innerHTML = "Username must be at least 5 characters."
+        errorusername.innerHTML = "Username must be at least 5 characters"
         username.value = ""
         flag = 1
     }
 
     if (password.value.length < 7){
-        errorpassword.innerHTML = "Password must be at least 7 characters."
+        errorpassword.innerHTML = "Password must be at least characters"
         password.value = ""
-        flag = 1
-    }
-
-    if (address.value.length < 20){
-        erroraddress.innerHTML = "Address must be longer"
-        address.value = ""
         flag = 1
     }
 
     if (flag == 0){
         window.location = "index.html"
     }
-
 
 })
